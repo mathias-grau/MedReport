2a4f190d425060f6d30b2b148ddff341e693b3dc
Mathias Grau <mathias4grau@gmail.com>
2025-01-03 18:14:49 +0100

    ras

--- data_preprocessing/extract_text.py
+++ data_preprocessing/extract_text.py
@@ -1,7 +1,18 @@
-def extract_text_from_pdf(pdf_path):
-    return "Simulated extracted text from PDF."
+from PyPDF2 import PdfReader
 
+def extract_text_from_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+    text = page.extract_text()
+    print(f"Extracted text from {number_of_pages} pages.")
+    return text
 
 
 def extract_text_from_png(png_path):
     return "Simulated extracted text from png."
+
+
+#### Test the functions
+preprocessed_text = extract_text_from_pdf("../data/exemple.pdf")
+print(preprocessed_text)
