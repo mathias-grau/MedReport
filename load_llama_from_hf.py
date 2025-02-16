5790517a35a0742c7ee0ebe41803989ccf4f3026
mathias-grau <mathias4grau@gmail.com>
2025-02-16 13:13:45 +0100

    ras

--- load_llama_from_hf.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B", cache_dir="./")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-3B", cache_dir="./")
