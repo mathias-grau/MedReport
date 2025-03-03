35d444a5ccc839d10fbe11bf5a7eaa9ef637e64e
Mathias Grau <mathias4grau@gmail.com>
2025-03-03 16:18:17 +0100

    come back

--- data_parsing/qa_singleton.py
+++ data_parsing/qa_singleton.py
@@ -10,7 +10,7 @@ def get_qa_pipeline():
     global _qa_pipeline
     if _qa_pipeline is None:
         print("Loading QA model and tokenizer into memory...")
-        tokenizer = AutoTokenizer.from_pretrained(Config.TOKENIZER_NAME, cache_dir='./app/')
-        model = AutoModelForQuestionAnswering.from_pretrained(Config.QA_MODEL_NAME, cache_dir='./app').to(Config.DEVICE)
+        tokenizer = AutoTokenizer.from_pretrained(Config.TOKENIZER_NAME, cache_dir='./')
+        model = AutoModelForQuestionAnswering.from_pretrained(Config.QA_MODEL_NAME, cache_dir='./').to(Config.DEVICE)
         _qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer, device=0 if Config.DEVICE == "cuda" else -1)
     return _qa_pipeline
